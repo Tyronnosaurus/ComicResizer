090678dd192f79331b0d2f6087361742be86f499
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2020-05-24 00:55:54 +0200

    Added Resize and Compress sections

--- ComicResizer.py
+++ ComicResizer.py
@@ -1,6 +1,16 @@
 import zipfile
 import os
-#import Image
+from PIL import Image
+
+
+#--- FUNCTIONS --------------------------
+def AddSuffix(filepath):
+    return (os.path.splitext(path_to_zip_file)[0]) + ".2.zip"
+
+
+#----------------------------------------
+
+
 
 print("Working...")
 
@@ -8,41 +18,49 @@ path_to_zip_file = 'C:\\Users\\Eduard\\Desktop\\AAA.zip'
 
 
 #Prepare temp folder name (it's just the filepath with the extension removed)
-directory_to_extract_to = (os.path.splitext(path_to_zip_file)[0])
+tempFolder = (os.path.splitext(path_to_zip_file)[0])
 
 
 #Extract
 zip_ref = zipfile.ZipFile(path_to_zip_file, 'r')
-zip_ref.extractall(directory_to_extract_to)
+zip_ref.extractall(tempFolder)
 zip_ref.close()
 
 
 
-
 #Resize
-"""
 basewidth = 1280
-img = Image.open('somepic.jpg')
-wpercent = (basewidth/float(img.size[0]))
-hsize = int((float(img.size[1])*float(wpercent)))
-img = img.resize((basewidth,hsize), Image.ANTIALIAS)
-img.save('sompic.jpg') 
-"""
+quality_val = 90
 
-
-
-
-
-
+for filename in os.listdir(tempFolder):
+   
+    filepath = os.path.join(tempFolder, filename)
+    img = Image.open(filepath)
+    wpercent = (basewidth/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+    img.save(filepath, 'JPEG', quality=quality_val)
 
 
 
 #Change format if necessary
 
+
 #Compress
+newFilepath = AddSuffix(path_to_zip_file)
+zf = zipfile.ZipFile(newFilepath, "w")
+for dirname, subdirs, files in os.walk(tempFolder):
+    zf.write(dirname)
+    for filename in files:
+        zf.write(os.path.join(dirname, filename))
+zf.close()
+
 
 #Delete temp directory
 
 #Rename/delete original file
 
 #Rename new file with original filename
+
+
+
