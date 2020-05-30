a4e4a5efb4d291d21a3e999a71aae14a7b786f6b
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2020-05-30 22:49:03 +0200

    Moved TODOs to separate file

--- ComicResizer.py
+++ ComicResizer.py
@@ -4,19 +4,10 @@ import shutil
 from ResizingFunctions import ResizeImagesInFolder
 from CompressionFunctions import *
 
-# TODOs
-# Only reduce size, not increase
-# Implement GIFs
-# Implement GUI or shell integration
-# Implement standalone images, not only zips
-# Rar support
-# If all files inside a root folder, move them to root
-
-
 
 print("Working...")
 
-oldFilepath = 'C:\\Users\\Eduard\\Desktop\\AAA.zip'
+oldFilepath = r'C:\Users\Eduard\Desktop\AAA.zip'
 
 ######################################################
 #Prepare temp folder name (it's just the filepath with the extension removed)
@@ -29,7 +20,7 @@ Extract(oldFilepath , tempFolder)
 
 ######################################################
 #Resize
-newWidth = 1280
+newWidth = 400 #1280
 ResizeImagesInFolder(tempFolder , newWidth)
 
 
