c266c4babdcd8bc1115e701ebb6c02f96363b6c4
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2020-08-05 01:00:17 +0200

    Context menu support for rar files

--- ContextMenu.py
+++ ContextMenu.py
@@ -9,6 +9,13 @@ arg='''Windows Registry Editor Version 5.00
 [HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.zip\shell\ComicResizer]
 [HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.zip\shell\ComicResizer\command]
 @="py D:/Continguts/ComicResizer/ComicResizer.pyw \\\"%1\\\""
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.RAR\shell]
+[HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.RAR\shell\ComicResizer]
+[HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.RAR\shell\ComicResizer\command]
+@="py D:/Continguts/ComicResizer/ComicResizer.pyw \\\"%1\\\""
+
+
 '''
 
 def AddToContextMenu():
