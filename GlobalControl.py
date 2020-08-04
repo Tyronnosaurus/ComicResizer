06031fdaced0ce9f5f4f171995f3b1faab21077e
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2020-08-04 21:04:01 +0200

    Deleted CompressFolderContents(), refactored a bit

--- GlobalControl.py
+++ GlobalControl.py
@@ -6,6 +6,8 @@ from send2trash import send2trash   #pip install Send2Trash
 import shutil
 import Misc
 import tkinter
+import sys
+
 
 '''Controls high level application flow'''
 
@@ -15,20 +17,18 @@ def ResizeComic(filePath, newWidth, settings):
     filePath = Misc.cleanPath(filePath)
     tempFolder = GetTempFolder(filePath)
 
-    if (Misc.IsArchive(filePath)):
-        #For zip and rar files, we extract contents to a temp folder, resize them, and compress them back
+    if (Misc.IsArchive(filePath)):  #For compressed files, we extract contents to a temp folder, resize them, and compress them back
         Compression.Extract(filePath, tempFolder)
         Resizer.ResizeImagesInFolder(tempFolder, newWidth, settings)
-        CompressFolderContents(filePath, tempFolder, settings)
+        if (settings.deleteOriginal.get()): send2trash(filePath)    #Delete original file
+        Compression.Zip(tempFolder, filePath)
+        if (settings.deleteTemp.get()): shutil.rmtree(tempFolder)  #Delete temp directory if necessary
 
-    elif (Misc.IsFolder(filePath)):
-        #For folders, resize all images inside
+    elif (Misc.IsFolder(filePath)): #For folders, just resize all images inside
         Resizer.ResizeImagesInFolder(filePath, newWidth, settings)
 
 
-    if (settings.closeWhenFinished.get()):
-        import sys
-        sys.exit(0)
+    if (settings.closeWhenFinished.get()): sys.exit(0)  #Exit application
 
 
 
@@ -53,7 +53,9 @@ def ResizeAndCompress(filePath, newWidth, settings):
     
     if (Misc.IsArchive(filePath)):
         Resizer.ResizeImagesInFolder(tempFolder, newWidth, settings)
-        CompressFolderContents(filePath, tempFolder, settings)
+        if (settings.deleteOriginal.get()): send2trash(filePath)    #Delete original file
+        Compression.Zip(tempFolder, filePath)
+        if (settings.deleteTemp.get()): shutil.rmtree(tempFolder)  #Delete temp directory if necessary
 
     else:
         print('Not a compressed file')
@@ -64,12 +66,4 @@ def ResizeAndCompress(filePath, newWidth, settings):
 def GetTempFolder(filePath):
     return(os.path.splitext(filePath)[0]) #Same name as filePath but without extension
 
-
-def CompressFolderContents(filePath, tempFolder, settings):
-    if (settings.deleteOriginal.get()):
-        send2trash(filePath)    #Delete original file
-
-    Compression.Zip(tempFolder , filePath)
-
-    if (settings.deleteTemp.get()):
-        shutil.rmtree(tempFolder)  #Delete temp directory
+ 
